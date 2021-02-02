c83fac1cce10a00c3b3d93ba017a8182fba907fb
maxchistt <maks.novohatsckij@yandex.ru>
2021-02-02 22:51:05 +0300

    Initial commit

---
+++ evolution/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace evolution
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button_start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_pause = new System.Windows.Forms.Button();
+            this.button_new_sim = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button_start
+            // 
+            this.button_start.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_start.Location = new System.Drawing.Point(472, 65);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(94, 29);
+            this.button_start.TabIndex = 0;
+            this.button_start.Text = "start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(191, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 38);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "amount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(191, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 38);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "amount";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(17, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 35);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Angry";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(17, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 35);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Peaceful";
+            // 
+            // button_pause
+            // 
+            this.button_pause.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_pause.Location = new System.Drawing.Point(572, 65);
+            this.button_pause.Name = "button_pause";
+            this.button_pause.Size = new System.Drawing.Size(94, 29);
+            this.button_pause.TabIndex = 7;
+            this.button_pause.Text = "pause";
+            this.button_pause.UseVisualStyleBackColor = true;
+            this.button_pause.Click += new System.EventHandler(this.button_pause_Click);
+            // 
+            // button_new_sim
+            // 
+            this.button_new_sim.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_new_sim.Location = new System.Drawing.Point(110, 65);
+            this.button_new_sim.Name = "button_new_sim";
+            this.button_new_sim.Size = new System.Drawing.Size(162, 29);
+            this.button_new_sim.TabIndex = 8;
+            this.button_new_sim.Text = "new simulation";
+            this.button_new_sim.UseVisualStyleBackColor = true;
+            this.button_new_sim.Click += new System.EventHandler(this.button_new_sim_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 28);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "10";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(197, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 28);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "10";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(374, 68);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 28);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "100";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(23, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "angry";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(197, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "peaceful";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.Control;
+            this.label7.Location = new System.Drawing.Point(374, 37);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "food per day";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(110, 120);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(556, 123);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " start amount of animals and food";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(110, 272);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(556, 154);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "simulation dynamics";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(395, 106);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(18, 20);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(395, 47);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 20);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Day:";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(110, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(556, 41);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "EVOLUTION SIMULATOR";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button_new_sim);
+            this.Controls.Add(this.button_pause);
+            this.Controls.Add(this.button_start);
+            this.Name = "Form1";
+            this.Text = "Evolution";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_pause;
+        private System.Windows.Forms.Button button_new_sim;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ evolution/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace evolution
+{
+    
+    public partial class Form1 : Form
+    {
+        public Simulation sim;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Stop();
+        }
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sim.playDay();
+            updSimInfo();
+            //Refresh();
+        }
+
+        void updSimInfo()
+        {
+            int angry = sim.getInfo(true);
+            int peaceful = sim.getInfo(false);
+            int day = sim.day;
+
+            this.label1.Text = angry.ToString();
+            this.label2.Text = peaceful.ToString();
+            this.label10.Text = day.ToString();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void newSimFromInput()
+        {
+            int ang, pea, fam;
+            ang = Convert.ToInt32(textBox1.Text);
+            pea = Convert.ToInt32(textBox2.Text);
+            fam = Convert.ToInt32(textBox3.Text);
+            sim = new Simulation(ang, pea, fam);
+        }
+
+        private void button_new_sim_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            newSimFromInput();
+            updSimInfo();
+        }
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            if (sim == null) {
+                timer1.Stop();
+                newSimFromInput();
+                updSimInfo();
+            }
+
+            timer1.Start();
+        }
+
+        private void button_pause_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+       
+    }
+
+    public class Simulation
+    {
+        Random rnd = new Random();
+        Match[] matchArr;
+        List <Animal> animalArr = new List<Animal>();
+
+        public int day = 0;
+
+        public Simulation(int angry_amount, int peaceful_amount, int food_amount)
+        {
+            this.matchArr = new Match[food_amount];
+
+            //first fill of animal arr
+            for (int i = 0; i < peaceful_amount+angry_amount; i++)
+            {
+                animalArr.Add(new Animal(i<angry_amount?true:false));
+            }
+
+        }
+
+        public void playDay()
+        {
+            List<Animal> newDayAnimalArr = new List<Animal>();
+            
+
+            //refresh match arr
+            for (int i = 0; i < matchArr.Length; i++)
+            {
+                matchArr[i] = new Match();
+            }
+
+            //fill matches
+            for (int j = 0; j < 2; j++)
+            {
+                for (int i = 0; i < matchArr.Length; i++)
+                {
+                    if (animalArr.Count > 0)
+                    {
+                        int aNum = rnd.Next(animalArr.Count);
+                        matchArr[i].add(animalArr[aNum]);
+                        animalArr.RemoveAt(aNum);
+
+                    }
+                    
+                }
+
+            }
+
+            //get results
+            for (int i = 0; i < matchArr.Length; i++)
+            {
+                List<Animal> addList = matchArr[i].getResult();
+                newDayAnimalArr.AddRange(addList);
+            }
+
+            //apply results
+            this.animalArr = newDayAnimalArr;
+
+            day++;
+        }
+
+        public int getInfo(bool angry)
+        {
+            int amount = 0;
+            foreach (var item in animalArr)
+            {
+                if (item.angryMod == angry) amount++;
+            }
+            return amount;
+        }
+
+        
+    }
+
+    public class Animal
+    {
+        public bool angryMod;
+        public Animal(bool Angry)
+        {
+            this.angryMod = Angry;
+        }
+    }
+
+    public class Match
+    {
+        List<Animal> matchAnimals = new List<Animal>();
+        Random rnd = new Random();
+        public Match()
+        {
+
+        }
+
+        public void add(Animal animal)
+        {
+            matchAnimals.Add(animal);
+        }
+
+        public List<Animal> getResult()
+        {
+            List<Animal> res = new List<Animal>();
+
+            if (matchAnimals.Count == 1)
+            {
+                res.Add(matchAnimals[0]);
+                res.Add(matchAnimals[0]);//children
+            }
+            else if (matchAnimals.Count > 1)
+            {
+                bool aMod0 = matchAnimals[0].angryMod;
+                bool aMod1 = matchAnimals[1].angryMod;
+
+                if (aMod0 != aMod1)
+                {
+                    int winnerNum = (aMod0) ? 0 : 1;
+                    int loserNum = (aMod0) ? 1 : 0;
+
+                    if (rnd.Next(2) > 0) res.Add(matchAnimals[loserNum]);//loser
+                    res.Add(matchAnimals[winnerNum]);//winner 
+                    if (rnd.Next(2) > 0) res.Add(matchAnimals[winnerNum]);//winner children
+
+                }
+                else if (aMod0 == true & aMod1 == true)
+                {
+                   res.Add(matchAnimals[rnd.Next(2)]);//fight winner
+               
+                }
+                else if (aMod0 == false & aMod1 == false)
+                {
+                    res.Add(matchAnimals[0]);//animal1
+                    if (rnd.Next(2) > 0) res.Add(matchAnimals[0]);//children1
+                    res.Add(matchAnimals[1]);//animal2
+                    if (rnd.Next(2) > 0) res.Add(matchAnimals[1]);//children2
+                }
+                
+
+
+            };
+
+
+            return res;
+        }
+
+    }
+}
